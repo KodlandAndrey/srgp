5ae7682102e524f7c6080bafb1a0b1c465dc21db
KodlandAndrey <146629404+KodlandAndrey@users.noreply.github.com>
2024-05-10 22:43:09 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,67 @@
+#Импорт
+from flask import Flask, render_template, request
+import webbrowser
+
+
+app = Flask(__name__)
+
+def result_calculate(size, lights, device):
+    #Переменные для энергозатратности приборов
+    home_coef = 100
+    light_coef = 0.04
+    devices_coef = 5   
+    return size * home_coef + lights * light_coef + device * devices_coef 
+
+#Первая страница
+def lights():
+    return render_template(
+                            'lindex.html'
+                           )
+#Вторая страница
+@app.route('/<size>')
+def lights(size):
+    return render_template(
+                            'lights.html', 
+                            size=size
+                           )
+
+#Третья страница
+@app.route('/<size>/<lights>')
+def electronics(size, lights):
+    return render_template(
+                            'electronics.html',                           
+                            size = size, 
+                            lights = lights                           
+                           )
+
+#Расчет
+@app.route('/<size>/<lights>/<device>')
+def end(size, lights, device):
+    return render_template('end.html', 
+                            result=result_calculate(int(size),
+                                                    int(lights), 
+                                                    int(device)
+                                                    )
+                        )
+#Форма
+@app.route('/form')
+def form():
+    return render_template('form.html')
+
+#Результаты формы
+@app.route('/submit', methods=['POST'])
+def submit_form():
+    #Создай переменные для сбора информации
+    name = request.form['name']
+    date = request.form['date']
+    email = request.form['email']
+    address = request.form['address']
+    # здесь вы можете сохранить данные или отправить их по электронной почте
+    return render_template('form_result.html', 
+                           #Помести переменные
+                           name=name,
+                           address=address,
+                           email=email,
+                           date=date)
+
+app.run(debug=True)
